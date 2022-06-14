01b1b8cf36a5b3b49527e69c784989fbcea80d8b
rogeriobonatti <rogeriobonatti@gmail.com>
2022-06-14 03:32:33 +0000

    eval models

--- mushr_rhc_ros/src/eval_all_models.py
+++ mushr_rhc_ros/src/eval_all_models.py
@@ -21,26 +21,26 @@ import pandas as pd
 #               '/home/azureuser/hackathon_data_premium/e2e_eval/12L1p0/info.csv',
 #               '/home/azureuser/hackathon_data_premium/e2e_eval/24L1p0/info.csv']
 
-file_paths = ['/home/azureuser/hackathon_data_premium/e2e_eval_models1/3L0p0/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/6L0p0/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/12L0p0/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/24L0p0/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/3L0p01/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/6L0p01/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/12L0p01/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/24L0p01/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/3L0p1/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/6L0p1/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/12L0p1/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/24L0p1/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/3L0p5/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/6L0p5/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/12L0p5/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/24L0p5/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/3L1p0/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/6L1p0/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/12L1p0/info.csv',
-              '/home/azureuser/hackathon_data_premium/e2e_eval_models1/24L1p0/info.csv']
+file_paths = ['/home/azureuser/hackathon_data_premium/e2e_eval_models4/3L0p0/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/6L0p0/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/12L0p0/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/24L0p0/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/3L0p01/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/6L0p01/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/12L0p01/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/24L0p01/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/3L0p1/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/6L0p1/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/12L0p1/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/24L0p1/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/3L0p5/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/6L0p5/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/12L0p5/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/24L0p5/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/3L1p0/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/6L1p0/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/12L1p0/info.csv',
+              '/home/azureuser/hackathon_data_premium/e2e_eval_models4/24L1p0/info.csv']
 
 all_vals_mean = np.zeros(shape=(4*5))
 all_vals_median = np.zeros(shape=(4*5))
@@ -56,20 +56,20 @@ for i, file_path in enumerate(file_paths):
     # times = data[100:,1]
 
     # clear data that crashes immediately
-    min_time = 20.0
+    min_time = 10.0
     condition = times>min_time
     distances = distances[condition]
     times = times[condition]
 
     # clear data above episode max
-    max_dist = 1000
+    max_dist = 1500
     condition = distances<max_dist
     distances = distances[condition]
     times = times[condition]
 
     plt.hist(distances, bins=30, facecolor='green', alpha=0.75)
     # plt.savefig('/home/azureuser/hackathon_data/e2e_eval/GPTiros_e2e_8gpu_2022-02-17_v2/fig.png')
-    plt.savefig('/home/azureuser/hackathon_data_premium/e2e_eval_models1/model_test/fig{}.png'.format(str(i)))
+    plt.savefig('/home/azureuser/hackathon_data_premium/e2e_eval_models4/model_test/fig{}.png'.format(str(i)))
     plt.show()
     plt.clf()
 
@@ -85,19 +85,22 @@ for i, file_path in enumerate(file_paths):
 
 # 'Num tokens': ['540', '30K', '300K', '1.5M', '3M']
 
-# data = {'3L':  all_vals_mean[::4],
-#         '6L':  all_vals_mean[1::4],
-#         '12L': all_vals_mean[2::4],
-#         '24L': all_vals_mean[3::4],
-#         # 'Dataset fraction': [0.0, 0.01, 0.1, 0.5, 1.0],
-#         'Dataset fraction': ['540', '30K', '300K', '1.5M', '3M']}
+all_vals_mean[:4] = [6.60,6.63,6.15,6.33]
+all_vals_median[:4] = [6.60,6.63,6.15,6.33]
 
-data = {'3L':  all_vals_median[::4],
-        '6L':  all_vals_median[1::4],
-        '12L': all_vals_median[2::4],
-        '24L': all_vals_median[3::4],
+data = {'3L':  all_vals_mean[::4],
+        '6L':  all_vals_mean[1::4],
+        '12L': all_vals_mean[2::4],
+        '24L': all_vals_mean[3::4],
         # 'Dataset fraction': [0.0, 0.01, 0.1, 0.5, 1.0],
         'Dataset fraction': ['540', '30K', '300K', '1.5M', '3M']}
+
+# data = {'3L':  all_vals_median[::4],
+#         '6L':  all_vals_median[1::4],
+#         '12L': all_vals_median[2::4],
+#         '24L': all_vals_median[3::4],
+#         # 'Dataset fraction': [0.0, 0.01, 0.1, 0.5, 1.0],
+#         'Dataset fraction': ['540', '30K', '300K', '1.5M', '3M']}
 
 # sns.lineplot(data=data, x="Dataset fraction", y=['6L', '12L', '24L'])
 
@@ -105,4 +108,4 @@ df = pd.DataFrame(data)
 print(df)
 dfm = df.melt('Dataset fraction', var_name='cols', value_name='Average meters traveled')
 sns.catplot(x="Dataset fraction", y="Average meters traveled", hue='cols', data=dfm, kind='point')
-plt.savefig('/home/azureuser/hackathon_data_premium/e2e_eval_models1/model_test/all_plots.png')
+plt.savefig('/home/azureuser/hackathon_data_premium/e2e_eval_models4/model_test/all_plots.png')
